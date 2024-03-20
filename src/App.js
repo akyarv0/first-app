04069ae4f7bc8578121e2708ad59c259fc7e9896
akyarv0 <akyarv0@gmail.com>
2024-03-20 23:02:33 +0300

    first react comps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
-import './App.css';
+import { Footer } from "./Footer";
+import Header from "./Header";
+import { Main } from "./Main";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello React!</h1>
+      <h1>APP</h1>
+      {/* Header comp invoke edildi */}
+      <Header />
+      <Main />
+      <Footer/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+//? Header Companent
+
+function Header() {
+  //? JS Alanı
+const yazi = "Hello FS16"
+
+  return (
+    <header>
+      {/* JSX yorum satırı */}
+      <h1>{yazi}</h1>
+    </header>
+  );
+}
+
+export default Header
+
+
